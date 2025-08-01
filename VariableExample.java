03381545a233437f934063166193d6efc486988f
JavaAnnathe <62948574+JavaAnnathe@users.noreply.github.com>
2025-08-01 20:39:34 +0530

    Add files via upload

---
+++ VariableExample.java
@@ -0,0 +1,16 @@
+package session1;
+
+public class VariableExample {	
+	
+	
+	public static void main(String arg[]) {
+		
+		int employeeNumber=3639;
+		
+		System.out.println("My employee number: "+employeeNumber);
+		
+		
+	}
+	
+
+}
