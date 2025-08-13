90f31761ba93e62875a22b5122b76f42432a0e92
Angelin207 <angelin207jery@gmail.com>
2025-08-13 19:38:37 +0530

    Add files via upload

---
+++ Assignment5.java
@@ -0,0 +1,11 @@
+
+public class Assignment5 {
+	public static void main(String[] args) {
+		int i;
+		String given = "Be happy";
+		for (i=1;i<=20;i++) {
+			System.out.println(given);
+		}
+	}
+
+}
